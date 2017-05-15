af64b6766e272d8de9cf1d8b3f4d359b86f0c9bd
Sean D. Hunt <seandhunt_7@yahoo.com>
2017-05-15 13:41:41 -0500

    Version bump to 0.0.4a0.

--- versioninfo.hpp
+++ versioninfo.hpp
@@ -8,12 +8,12 @@
 /* version information. */
 #define PTHMGR_MAJOR 0
 #define PTHMGR_MINOR 0
-#define PTHMGR_RELEASE 3
+#define PTHMGR_RELEASE 4
 #define PTHMGR_BUILD 0
 
 /* Main version info things. */
 #define PTHMGR_DESCRIPTION "Windows(R) Path Environment Variable Manager"
-#define PTHMGR_VERSION "0.0.3"
+#define PTHMGR_VERSION "0.0.4a0"
 #define INTERNAL_NAME "pthmgr"
 #define LEGAL_COPYRIGHT "Copyright (C) 2017"
 #define ORIGINAL_FILENAME "pthmgr.exe"
